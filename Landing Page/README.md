995fd47430ec80f175b86204bbc25462868bf6e5
Bardala <102490981+Bardala@users.noreply.github.com>
2022-10-25 08:01:31 +0200

    Add files via upload

---
+++ Landing Page/README.md
@@ -0,0 +1,26 @@
+# Landing page project
+
+## Table of Contents
+
+- Html and css mostly from udacity's startup project but i did some changes
+- use Dom to generate a navbar
+- smooth scroll with js
+- add and remove active class when scrolling through the sections
+- editing some css and html
+
+## sources i used
+
+- https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+- https://developer.mozilla.org/en*US/docs/Web/API/Element/getBoundingClientRect
+- https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
+- https://developer.mozilla.org/en*US/docs/Web/API/HTMLElement/dataset
+- https://developer.mozilla.org/en*US/docs/Web/API/Element#instance_properties
+- https://developer.mozilla.org/en*US/docs/Web/API/Element/scrollIntoView
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+- https://developer.mozilla.org/en-US/docs/Web/API/Element/focus_event
+- https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseover_event
+- https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events#other_event_listener_mechanisms
+
+## Author's name
+
+- Islam Bardala
---
+++ Landing Page/js/app.js
@@ -0,0 +1,157 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const sections = document.querySelectorAll("section");
+// list acts the <ul> tag in HTML in nav
+let list = document.querySelector("#navbar__list");
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+function itemIdFromSectionId(sectionId) {
+  return sectionId.replace(/\bsection/gi, "item");
+}
+function sectionIdFromItemId(itemId) {
+  return itemId.replace(/\bitem/gi, "section");
+}
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+//first way by dom methods no html code !(innerHTML)method
+/* function createNavList() {
+    sections.forEach((section) => {
+      let newItem = document.createElement("li");
+      let link = document.createElement("a");
+      link.classList.add("menu__link");
+      link.setAttribute("href", `#${section.id}`);
+      //https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+      link.innerHTML = section.dataset.nav;
+      newItem.appendChild(link);
+      list.appendChild(newItem);
+    });
+  } */
+createNavList();
+// Another way by html code it is more readable (innerHTML)method
+function createNavList() {
+  let htmlCode = "";
+
+  sections.forEach((sec) => {
+    htmlCode += `<li class="list"><a id="${itemIdFromSectionId(
+      sec.id
+    )}" class="menu__link" class="">${sec.dataset.nav}</a>`;
+  });
+  list.innerHTML = htmlCode;
+}
+
+// Scroll to section on link click
+list.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  let target = evt.target;
+  if (target.className === "menu__link") {
+    targetSection = document.querySelector(
+      `#${sectionIdFromItemId(target.id)}`
+    );
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// Add class 'active' to section when near top of viewport
+
+// Set sections as active
+function sectionActivation(section) {
+  let secId = section.getAttribute("id");
+  let sec = document.getElementById(`${secId}`);
+  sec.classList.add("your-active-class");
+  document
+    .querySelector(`#${itemIdFromSectionId(secId)}`)
+    .classList.add("your-active-class");
+}
+// remove active from sections
+function sectionDeactivation(section) {
+  let secId = section.getAttribute("id");
+  let sec = document.getElementById(`${secId}`);
+  sec.classList.remove("your-active-class");
+  document
+    .querySelector(`#${itemIdFromSectionId(secId)}`)
+    .classList.remove("your-active-class");
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+/**The Element.getBoundingClientRect() method returns a DOMRect object
+ * providing information about the size of an element and its position relative
+ * to the viewport. */
+function updateSectionState() {
+  sections.forEach((sec) => {
+    if (
+      sec.getBoundingClientRect().top <= 150 &&
+      sec.getBoundingClientRect().bottom >= 150
+    )
+      sectionActivation(sec);
+    else sectionDeactivation(sec);
+  });
+}
+// Scroll to anchor ID using scrollTO event
+document.addEventListener("scroll", updateSectionState);
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// list.addEventListener(
+//   "mouseover",
+//   (evt) => {
+//     evt.preventDefault();
+//     let target = evt.target;
+//     if (target.className === "menu__link") {
+//       targetSection = document.querySelector(
+//         `#${sectionIdFromItemId(target.id)}`
+//       );
+//       target.style.background = "#333";
+//       setTimeout(() => {
+//         target.style.color = "";
+//       }, 500);
+//     }
+//   },
+//   false
+// );
+
+// list.addEventListener("blur", (evt) => { // vs 'focus'
+//   evt.preventDefault();
+//   let target = evt.target;
+//   if (target.className === "menu__link") {
+//     targetSection = document.querySelector(
+//       `#${sectionIdFromItemId(target.id)}`
+//     );
+//     target.style.background = "";
+//   }
+// });
