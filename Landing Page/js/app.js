5bb40116494e62b4018a42158c667f9cb626ca8a
Bardala <102490981+Bardala@users.noreply.github.com>
2022-10-25 08:19:57 +0200

    Update app.js

--- Landing Page/js/app.js
+++ Landing Page/js/app.js
@@ -36,6 +36,22 @@ function itemIdFromSectionId(sectionId) {
 function sectionIdFromItemId(itemId) {
   return itemId.replace(/\bitem/gi, "section");
 }
+function sectionActivation(section) {
+  let secId = section.getAttribute("id");
+  let sec = document.getElementById(`${secId}`);
+  sec.classList.add("your-active-class");
+  document
+    .querySelector(`#${itemIdFromSectionId(secId)}`)
+    .classList.add("your-active-class");
+}
+function sectionDeactivation(section) {
+  let secId = section.getAttribute("id");
+  let sec = document.getElementById(`${secId}`);
+  sec.classList.remove("your-active-class");
+  document
+    .querySelector(`#${itemIdFromSectionId(secId)}`)
+    .classList.remove("your-active-class");
+}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -43,21 +59,20 @@ function sectionIdFromItemId(itemId) {
  */
 
 // build the nav
-//first way by dom methods no html code !(innerHTML)method
+//first way by just dom methods no html code 
 /* function createNavList() {
     sections.forEach((section) => {
       let newItem = document.createElement("li");
       let link = document.createElement("a");
       link.classList.add("menu__link");
       link.setAttribute("href", `#${section.id}`);
-      //https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
       link.innerHTML = section.dataset.nav;
       newItem.appendChild(link);
       list.appendChild(newItem);
     });
   } */
 createNavList();
-// Another way by html code it is more readable (innerHTML)method
+// Another way by html code it is more readable
 function createNavList() {
   let htmlCode = "";
 
@@ -70,30 +85,6 @@ function createNavList() {
 }
 
 // Add class 'active' to section when near top of viewport
-
-// Set sections as active
-function sectionActivation(section) {
-  let secId = section.getAttribute("id");
-  let sec = document.getElementById(`${secId}`);
-  sec.classList.add("your-active-class");
-  document
-    .querySelector(`#${itemIdFromSectionId(secId)}`)
-    .classList.add("your-active-class");
-}
-// remove active from sections
-function sectionDeactivation(section) {
-  let secId = section.getAttribute("id");
-  let sec = document.getElementById(`${secId}`);
-  sec.classList.remove("your-active-class");
-  document
-    .querySelector(`#${itemIdFromSectionId(secId)}`)
-    .classList.remove("your-active-class");
-}
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
-/**The Element.getBoundingClientRect() method returns a DOMRect object
- * providing information about the size of an element and its position relative
- * to the viewport. */
 function updateSectionState() {
   sections.forEach((sec) => {
     if (
@@ -108,7 +99,6 @@ function updateSectionState() {
 /**
  * End Main Functions
  * Begin Events
- *
  */
 
 // Scroll to section on link click
